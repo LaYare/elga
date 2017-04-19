de065d1431dcc35d484fc2c47962a27407a276c7
YareArista <yarearista@gmail.com>
2017-04-19 16:19:23 -0500

    diseño responsive

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -9,7 +9,7 @@
   }]);
 
    /** @ngInject */
-  function MainController($anchorScroll, $location, $scope) {
+  function MainController($anchorScroll, $location, $scope, $http, $window) {
     $scope.gotoAnchor = function(x) {
       var newHash = 'anchor' + x;
       if ($location.hash() !== newHash) {
@@ -22,12 +22,33 @@
         $anchorScroll();
       }
     };
+
+    // registro
+    $scope.user = {};
+
+    $scope.addPost = function () {
+       // $scope.user.page = "mellowventures.mx";
+       $scope.user.name = " ";
+       $scope.user.email = " ";
+       $scope.user.telephone = " ";
+       $scope.user.message = " ";
+
+       $http.post("http://emailapi.mellow.online/api/general",$scope.user)
+           .success(function (data, status, headers, config) {
+               $window.location.reload();
+           })
+           .error(function (data, status, header, config) {
+                console.log(data);
+           });
+
+    };
   }
 })();
 
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
+
     if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
       document.getElementById("menu-s").className = "menu-down";
     }
@@ -48,7 +69,7 @@ function myFunction() {
 
     };
 
-}
+};
 
 // efecto maquina de escribir
 var mensaje = "Controla el consumo de gas en tu negocio u hogar de una manera inteligente", pausa = 90; 
